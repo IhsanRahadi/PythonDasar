be461eb2e3ba94cc33e12e16d6c35d236f863e0e
IhsanRahadi <144403359+IhsanRahadi@users.noreply.github.com>
2023-09-08 11:09:40 +0700

    Add files via upload

---
+++ 3_variabel_string.py
@@ -0,0 +1,10 @@
+x = "jhon"
+x= 'jhon'
+
+
+a = 4
+A = "sally"
+
+print(x)
+print(a)
+print(A)
---
+++ 59_title_helloWorld.py
@@ -0,0 +1,5 @@
+# hello_world.py
+
+import pySimpleGUI as sg
+
+sg.Window(title="Hello world", layout=[[]], margins=(100, 50)).read()
